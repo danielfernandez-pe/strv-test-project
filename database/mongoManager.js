184add69a6274679d5a723f48b4fa7754e8cad70
Daniel <dfernandezsw@gmail.com>
2024-11-21 12:08:28 +0100

    Improving database uri in .env file

--- database/mongoManager.js
+++ database/mongoManager.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import logger from '../utils/logger.js';
 
 async function connectMongo() {
-    const uri = process.env.DATABASE_DEV_URI;
-    await mongoose.connect(uri);
+    // const dbUri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}${process.env.DATABASE_OPTIONS}`;
+    await mongoose.connect(process.env.DATABASE_DEV_URI);
     logger.info('Connection with MongoDB successful');
 }
 
