ab7ac8ffeb983a14fa768c9cee251c1e14ca71b2
Daniel <dfernandezsw@gmail.com>
2024-11-25 13:22:14 +0100

    Passing correct uri for mongo

--- src/init/mongoInit.ts
+++ src/init/mongoInit.ts
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 import logger from '../utils/logger';
 
 async function connectMongo() {
-    // const dbUri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}${process.env.DATABASE_OPTIONS}`;
-    await mongoose.connect(process.env.DATABASE_DEV_URI || '');
+    const dbUri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/${process.env.DATABASE_NAME}${process.env.DATABASE_OPTIONS}`;
+    await mongoose.connect(dbUri);
     logger.info('Connection with MongoDB successful');
 }
 
